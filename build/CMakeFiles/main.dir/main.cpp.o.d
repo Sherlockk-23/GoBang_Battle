86cb0775942700af478766afda5e9f6d599e359e
Sherlockk-23 <145881280+Sherlockk-23@users.noreply.github.com>
2023-11-20 08:55:21 +0800

    Delete build directory

--- build/CMakeFiles/main.dir/main.cpp.o.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/main.dir/main.cpp.o: /home/qinzk23/GoBang_Battle/main.cpp \
- /usr/include/stdc-predef.h /home/qinzk23/GoBang_Battle/include/board.h \
- /home/qinzk23/GoBang_Battle/include/game.h \
- /home/qinzk23/GoBang_Battle/include/board.h \
- /home/qinzk23/GoBang_Battle/include/player.h \
- /home/qinzk23/GoBang_Battle/include/strategy.h \
- /home/qinzk23/GoBang_Battle/include/robot1.h \
- /home/qinzk23/GoBang_Battle/include/player.h
